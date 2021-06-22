e1f4405e35071c88127d844ecdc8d0e65666ad4e
Mahmoud Abdel Wahab <mahmoud3799@gmail.com>
2021-06-22 10:57:30 +0200

    update seed script

--- saas/seed.ts
+++ saas/seed.ts
@@ -3,22 +3,24 @@ import * as faker from "faker";
 const prisma = new PrismaClient();
 
 const plans: PlanType[] = ["FREE", "PREMIUM"];
+const NUMBER_OF_USERS = 4;
+const NUMBER_OF_INVITES = 4;
 
-const data = Array.from({ length: 2 }).map(() => ({
+const data = Array.from({ length: NUMBER_OF_USERS }).map(() => ({
   email: faker.internet.email(),
   name: faker.name.firstName(),
   account: {
-    plan: plans[Math.floor(Math.random() * plans.length)], // pick random plan
+    plan: plans[faker.datatype.number({ min: 0, max: plans.length - 1 })], // pick random plan
     stripeCustomerId: faker.datatype.uuid(),
     stripeSubscriptionId: faker.datatype.uuid(),
     isActive: true,
   },
-  invites: Array.from({ length: Math.floor(Math.random() * 4) + 1 }).map(
-    () => ({
-      email: faker.internet.email(),
-      dateSent: faker.date.future(),
-    })
-  ),
+  invites: Array.from({
+    length: faker.datatype.number({ min: 0, max: NUMBER_OF_INVITES }),
+  }).map(() => ({
+    email: faker.internet.email(),
+    dateSent: faker.date.future(),
+  })),
 }));
 
 export const seed = async () => {
