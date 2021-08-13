0adeb7f21028bdffc6db997739db032e51b2f6ff
ruheni <aleruheni@outlook.com>
2021-08-13 15:58:51 +0300

    refactor

--- .github/scripts/tests/test.spec.ts
+++ .github/scripts/tests/test.spec.ts
@@ -25,19 +25,19 @@ const projects = [
 
 describe('Seed and run script', () => {
   test.each(projects)('$templateName against Postgres', async ({ templateName }) => {
-    const options = { cwd: `../../${templateName}` }
+    const execaConfig:execa.SyncOptions = { cwd: `../../${templateName}` }
 
-    execa.commandSync(` yarn`, options)
+    execa.commandSync(` yarn`, execaConfig)
 
-    execa.commandSync(`yarn prisma migrate reset --force`, options)
+    execa.commandSync(`yarn prisma migrate reset --force`, execaConfig)
 
-    const dbPush = execa.commandSync(`yarn prisma db push --schema prisma/schema.prisma`, options)
+    const dbPush = execa.commandSync(`yarn prisma db push --schema prisma/schema.prisma`, execaConfig)
     expect(dbPush.exitCode).toBe(0)
 
-    const seed = execa.commandSync(`yarn prisma db seed --preview-feature --schema prisma/schema.prisma`, options)
+    const seed = execa.commandSync(`yarn prisma db seed --preview-feature --schema prisma/schema.prisma`, execaConfig)
     expect(seed.exitCode).toBe(0)
 
-    const script = execa.commandSync(`yarn run dev`, options)
+    const script = execa.commandSync(`yarn run dev`, execaConfig)
     expect(script.exitCode).toBe(0)
   })
 })
